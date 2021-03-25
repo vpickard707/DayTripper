303b582deec49b86514af21645ff4fa69571d268
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-24 17:05:52 -0700

    updated time blocks and local storage

--- script.js
+++ script.js
@@ -36,4 +36,28 @@ $(document).ready(function () {
     });
   }
   currentTime();
+  //Set up local storage to store persistent changes in events by timeblock
+
+  button.on("click", function (event) {
+    event.preventDefault();
+    var text = $(this).siblings(".col-sm-10").val().replace(/['"]+/g, "");
+    var parent = $(this).parent().attr("id");
+    localStorage.setItem(parent, JSON.stringify(text));
+  });
+
+  $("#9 textarea").val(localStorage.getItem("9").replace(/['"]+/g, ""));
+  $("#10 textarea").val(localStorage.getItem("10").replace(/['"]+/g, ""));
+  $("#11 textarea").val(localStorage.getItem("11").replace(/['"]+/g, ""));
+  $("#12 textarea").val(localStorage.getItem("12").replace(/['"]+/g, ""));
+  $("#13 textarea").val(localStorage.getItem("13").replace(/['"]+/g, ""));
+  $("#14 textarea").val(localStorage.getItem("14").replace(/['"]+/g, ""));
+  $("#15 textarea").val(localStorage.getItem("15").replace(/['"]+/g, ""));
+  $("#16 textarea").val(localStorage.getItem("16").replace(/['"]+/g, ""));
+  $("#17 textarea").val(localStorage.getItem("17")).replace(/['"]+/g, "");
+
+  //Use styling to transform pointer into a disk icon at the time of click event (saving of event)
+
+  button.on("click", function (event) {
+    $("selector").css("cursor", "i");
+  });
 });
